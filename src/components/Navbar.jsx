5fb1cc5289b6bc26054f12b4bd6c3590603fb658
Ataby <matabay200069@gmail.com>
2024-03-07 23:26:59 +0300

    night & day feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,23 @@ import React ,{useState,useEffect} from 'react'
 import { MdOutlineDarkMode } from "react-icons/md";
 import { FiShoppingBag } from "react-icons/fi";
 import { IoMdMoon } from "react-icons/io";
-
+import { useDispatch } from 'react-redux';
+ 
 
 
 
 const Navbar = () => {
     const [color, setcolor] = useState(false);
+    const dispatch = useDispatch();
 
    useEffect(() => {
-    const root = document.querySelector("#root");
+    const night = document.querySelector(".night");
      if(color){
-        root.style.backgroundColor="black";
-        root.style.color="gray";
+        night.style.backgroundColor="black";
+        night.style.color="lightgray";
      }else {
-        root.style.backgroundColor="gray";
-        root.style.color="black";
+        night.style.backgroundColor="lightgray";
+        night.style.color="black";
      }
    
      
@@ -25,11 +27,11 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between border-2 px-2 h-28'>
-        <div>Logo</div>
+        <div className='text-2xl tracking-wider'>LOGO</div>
         <div className='flex border-2 items-center'>
             
             <input type="text" placeholder='search' className='p-2 outline-none rounded-lg' />
-            <div className='relative p-2'>
+            <div className='relative p-2' onClick={()=>dispatch({type:"DRAWER",payload:true})}>
                 <span className='bg-blue-200 absolute bottom-0' fontSize="" > 4</span>
                 <FiShoppingBag fontSize="32" className='cursor-pointer'/>
             </div>
---
+++ src/containers/PageContainers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageContainers = ({children}) => {
+  return (
+    <div className='  m-auto bg-gray-100'>{children}</div>
+  )
+}
+
+export default PageContainers 
