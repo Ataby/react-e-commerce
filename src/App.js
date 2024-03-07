27f5d3f95b37368d728dcdff25ca06e583475e13
Ataby <matabay200069@gmail.com>
2024-03-07 23:25:36 +0300

    Router & drawer elemnt

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 import PageContainers from "./containers/PageContainers";
+import { useSelector } from "react-redux";
+import Card from "./components/Card";
 
 
 function App() {
+  const {drawer}=useSelector(state => state.drawer);
+  console.log(drawer)
   return (
     <div className="App">
       <PageContainers>
@@ -17,6 +21,7 @@ function App() {
           <Route index element={<Home/>} />  
           <Route path="detail" element={<Details/>} />  
         </Routes>
+        {drawer && <Card/>}
         <Footer/>
       </BrowserRouter>
       </PageContainers>
