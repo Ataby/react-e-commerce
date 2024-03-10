a47a57576a3fd14ffce75e3dc3a21797293135cd
Ataby <matabay200069@gmail.com>
2024-03-10 22:59:04 +0300

    combined reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,18 +3,23 @@ import {composeWithDevTools} from "redux-devtools-extension"
 import {thunk} from "redux-thunk"
 import drawerReducer from "./reducers/drawer"
 import productsReducer from "./reducers/products"
-import productsDetail from "./reducers/productDetail"
+import productDetail from "./reducers/productDetail"
+import cartReducer from "./reducers/basket"
+import searchReducer from "./reducers/search"
 
 
+const cartItems = JSON.parse(localStorage.getItem("cartItems")) || []
 
 let initialState = {
-
+    cart:{cartItems},
 }
 
 const reducer = combineReducers({
     drawer:drawerReducer,
     products:productsReducer,
-    detail:productsDetail
+    product:productDetail,
+    cart:cartReducer,
+    search:searchReducer
 })
 
 const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(thunk)))
